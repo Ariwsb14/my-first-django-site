5dc922e82ec3476fa6990a0a75051c3750bdca1a
Ariwsb14 <Ariasb1385@gmail.com>
2024-10-13 18:36:53 +0330

    loading travel template

--- website/views.py
+++ website/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 
 
 def first_text(request):
-    return render(request , 'index.html')
+    return render(request , 'website/index.html')
 # Create your views here.
