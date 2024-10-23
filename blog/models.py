6d343b19755c86448c0b84b93440dae77a713119
Ariwsb14 <Ariasb1385@gmail.com>
2024-10-23 20:07:15 +0330

    making contacts and post databses
    + showing posts in admin panel
    + changing __str__

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    class Meta :
+        ordering = ['-created_date']
+    def __str__(self):
+        return '%s - %s' % (self.id , self.title)  
+
+
 # Create your models here.
