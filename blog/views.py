fa4c9957a52d1943fdb5e685ec9dc232404ea800
Ariwsb14 <Ariasb1385@gmail.com>
2024-10-24 12:56:48 +0330

    fasl 6 - tamrin1
    +showing dynamic content in blog hom page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from blog.models import Post
+import datetime
 
 def home(request):
-    return render(request , 'blogs/blog-home.html')
+    posts = Post.objects.filter(published_date__lte=datetime.datetime.now() , status=True)
+    context = {'posts':posts}
+    return render(request , 'blogs/blog-home.html' , context)   
+
 
 def single(request):
     return render(request , 'blogs/blog-single.html')
