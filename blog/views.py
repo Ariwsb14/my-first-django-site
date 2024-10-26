d5a929a1cbc46b9ab29160b180f7e9334f6716ee
Ariwsb14 <Ariasb1385@gmail.com>
2024-10-26 22:27:59 +0330

    end of fasl6-tamrin2
    +perv and next button in single post

--- blog/views.py
+++ blog/views.py
@@ -9,8 +9,32 @@ def home(request):
 
 
 def single(request,pid):
+    posts = Post.objects.filter(published_date__lte=datetime.datetime.now(),status=True)
     post = get_object_or_404(Post, pk=pid, status=True)
-    context = {'post':post}
+    index = 0
+    for i in range(len(posts)):
+        if posts[i].id == post.id:
+            index = i
+
+    tedad = len(posts)
+    perv = None
+    if index+2 <= tedad:
+        perv = posts[index+1]
+    else:
+        None  
+
+    next = None
+    if index > 0:
+        next = posts[index-1]
+    else:
+        None    
+         
+        
+    
+    
+        
+    
+    context = {'post':post , 'index':index , 'perv': perv , 'next':next }
     return render(request , 'blogs/blog-single.html', context)
 
 # Create your views here.
