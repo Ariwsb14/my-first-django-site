23f0deba2dfde75261ce033d2bc0509e7c549534
Ariwsb14 <Ariasb1385@gmail.com>
2024-11-03 09:29:30 +0330

    fasl7-tamrin1
    +home lastposts container

--- website/templatetags/home_tags.py
+++ website/templatetags/home_tags.py
@@ -1,3 +1,13 @@
 from django import template
+from blog.models import Post
+import datetime
 
 register = template.Library()
+
+@register.inclusion_tag('website/home-lastposts.html')
+def lastposts():
+    posts = Post.objects.filter(status=True , published_date__lte=datetime.datetime.now())
+    last_posts  = sorted(posts, key=lambda x: x.published_date , reverse=True)[:6]
+    return {'lastposts':last_posts}
+
+
