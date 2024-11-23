414d68a77229c8202cd51a6ea8ec2ea2c7554141
Ariwsb14 <160019638+Ariwsb14@users.noreply.github.com>
2024-11-23 19:20:33 +0330

    making comments classs
    + i models

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class category(models.Model):
 
     def __str__(self):
         return self.name
-
+    
 
 class Post(models.Model):
     image = models.ImageField(upload_to='blog/' , default='media/blog/default.jpg')
@@ -30,6 +30,20 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:single', kwargs={'pid':self.id}) 
+
+class coment(models.Model):
+    post = models.ForeignKey(Post , on_delete= models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()  
+    subject = models.CharField(max_length=255)
+    message = models.TextField() 
+    status = models.BooleanField(default= False) 
+    created_date = models.DateTimeField(auto_now_add= True)
+    published_date = models.DateTimeField(auto_now= True)
+
+    def __str__(self) :
+        return  '%s - %s'% (self.post , self.name)
+    
 
 
 
